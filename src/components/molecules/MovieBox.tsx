e37a7ba94d9b090f9d73226601494d2c213f34a1
Federico Moretti <dj.ikos90@gmail.com>
2019-07-25 23:30:53 +0200

    feat: improve movie box

--- src/components/molecules/MovieBox.tsx
+++ src/components/molecules/MovieBox.tsx
@@ -4,16 +4,27 @@ import { Movie } from '../../commons/types';
 import Title from '../atoms/Title';
 import Button from '../atoms/Button';
 import LabeledValue from './LabeledValue';
+import Image from '../atoms/Image';
+
+const mdbImagePath = 'https://image.tmdb.org/t/p/w200';
 
 const boxStyle = {
   transition: 'height 0.5s ease-in',
 };
 
-const titleWrapperStyle = {
+const infoContainerStyle = {
+  display: 'flex',
+  justifyContent: 'space-between',
+  marginTop: 20,
+};
+
+const titleContainerStyle = {
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
 };
+
+const imageStyle = { maxWidth: 200, minWidth: 120, borderRadius: 6 };
 
 interface MovieBoxProps {
   movie: Movie;
@@ -28,7 +39,7 @@ function MovieBox(props: MovieBoxProps) {
 
   return (
     <Box style={boxStyle}>
-      <div style={titleWrapperStyle}>
+      <div style={titleContainerStyle}>
         <Title style={{ marginBottom: 0 }} level={1} size={5} alignment="left">
           {movie.title} ({movie.releaseDate.slice(0, 4)})
         </Title>
@@ -37,7 +48,17 @@ function MovieBox(props: MovieBoxProps) {
         </Button>
       </div>
       {showInfos && (
-        <LabeledValue label="Vote:" value={movie.voteAverage.toString()} />
+        <div style={infoContainerStyle}>
+          <div>
+            <LabeledValue label="Vote:" value={movie.voteAverage.toString()} />
+            <LabeledValue label="Genres:" value={movie.genreIds.join(', ')} />
+            <LabeledValue label="Plot:" value={movie.overview} />
+          </div>
+          <Image
+            imageStyle={imageStyle}
+            src={`${mdbImagePath}${movie.posterPath}`}
+          />
+        </div>
       )}
     </Box>
   );
