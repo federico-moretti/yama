5e60587fd050e9df99ce5a6c94faacd9c6908fff
Federico Moretti <dj.ikos90@gmail.com>
2019-07-27 12:46:31 +0200

    feat: add movie poster constant

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -1,2 +1,3 @@
 export const MOVIE_DB_KEY = process.env.REACT_APP_MOVIE_DB_KEY;
 export const MOVIE_DB_PATH = process.env.REACT_APP_MOVIE_DB_PATH;
+export const MOVIE_DB_IMAGE_PATH = process.env.REACT_APP_MOVIE_DB_IMAGE_PATH;
