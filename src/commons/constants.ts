45c0b88accd8c82c048d96be8e9472f645834060
Federico Moretti <dj.ikos90@gmail.com>
2019-07-28 18:29:03 +0200

    feat: add warning if missing contants

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -1,3 +1,15 @@
 export const MOVIE_DB_KEY = process.env.REACT_APP_MOVIE_DB_KEY;
 export const MOVIE_DB_PATH = process.env.REACT_APP_MOVIE_DB_PATH;
 export const MOVIE_DB_IMAGE_PATH = process.env.REACT_APP_MOVIE_DB_IMAGE_PATH;
+
+function warnForUndefinedConstants(label: string, value: any) {
+  if (typeof value === 'undefined') {
+    console.warn(
+      `"${label}" is undefined, the app could have an unexpected behaviour.`
+    );
+  }
+}
+
+warnForUndefinedConstants('MOVIE_DB_KEY', MOVIE_DB_KEY);
+warnForUndefinedConstants('MOVIE_DB_PATH', MOVIE_DB_PATH);
+warnForUndefinedConstants('MOVIE_DB_IMAGE_PATH', MOVIE_DB_IMAGE_PATH);
